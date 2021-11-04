6a17576180f8620839056209514ad929c8a1587e
Jackson <lordjackson@gmail.com>
2021-11-04 14:33:51 -0300

    Conexão com Banco de Dados em Java Swing - Insert e Listar

--- SistemaEscolar/src/controller/AlunoController.java
+++ SistemaEscolar/src/controller/AlunoController.java
@@ -1,6 +1,12 @@
 package controller;
 
 import database.Conexao;
+import model.Aluno;
+
+import javax.swing.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AlunoController {
     private Conexao bd;
@@ -9,8 +15,47 @@ public class AlunoController {
         this.bd = new Conexao();
     }
 
+    public void salvar(Aluno aluno) {
+        try {
+            String sql;
+            sql = "INSERT INTO aluno (nome, matricula, dt_nascimento) VALUES(?,?,?)";
+            PreparedStatement stmt = this.bd.getConexao().prepareStatement(sql);
+
+            stmt.setString(1, aluno.getNome());
+            stmt.setString(2, aluno.getMatricula());
+            stmt.setString(3, aluno.getDt_nascimento());
+            stmt.execute();
+            stmt.close();
+
+        } catch (SQLException u) {
+            throw new RuntimeException(u);
+        }
+    }
+
     public void testarConexao() {
         this.bd.getConexao();
         System.out.println("Conectou");
+    }
+
+    public ResultSet listar() {
+        try {
+
+            //ArrayList dados = new ArrayList();
+            PreparedStatement ps = bd.getConexao().prepareStatement("SELECT * FROM aluno");
+            ResultSet rs = ps.executeQuery();
+            /*
+            while (rs.next()) {
+                System.out.println(rs.getInt("id"));
+                System.out.println(rs.getString("nome"));
+                System.out.println(rs.getString("matricula"));
+                System.out.println(rs.getString("dt_nascimento"));
+            }*/
+            // System.out.println(dados);
+            return rs;
+        } catch (SQLException e) {
+            e.getMessage();
+            JOptionPane.showMessageDialog(null, "Erro preencher o ArrayList");
+            return null;
+        }
     }
 }
--- SistemaEscolar/src/view/AlunoFormList.java
+++ SistemaEscolar/src/view/AlunoFormList.java
@@ -1,10 +1,13 @@
 package view;
 
 import controller.AlunoController;
+import model.Aluno;
+import net.proteanit.sql.DbUtils;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 
 public class AlunoFormList {
     private JPanel panelMain;
@@ -13,6 +16,10 @@ public class AlunoFormList {
     private JTextField nomeTxt;
     private JTextField dt_nascimentoTxt;
     private JTextField matriculaTxt;
+    private JButton listarBtb;
+    private JTable alunoJTable;
+    private JPanel panelTable;
+    String[] colunaNomes = {"Employee 1", "Employee 2"};
 
 
     public AlunoFormList() {
@@ -20,12 +27,37 @@ public class AlunoFormList {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                AlunoController alunoController = new AlunoController();
-                alunoController.testarConexao();
+                Aluno aluno = new Aluno();
+                aluno.setNome(nomeTxt.getText());
+                aluno.setMatricula(matriculaTxt.getText());
+                aluno.setDt_nascimento(dt_nascimentoTxt.getText());
 
-                System.out.println("Cliquei no botão");
+                AlunoController alunoController = new AlunoController();
+                alunoController.salvar(aluno);
+
+                //alunoController.testarConexao();
+                //System.out.println("Cliquei no botão");
             }
         });
+        listarBtb.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AlunoController alunoController = new AlunoController();
+                ResultSet rs = alunoController.listar();
+
+                alunoJTable.setModel(DbUtils.resultSetToTableModel(rs));
+
+                setNomeColuna(new String[]{"ID", "Matricula", "Nome", "Data Nascimento"});
+            }
+        });
+    }
+
+
+    public void setNomeColuna(String[] colunas) {
+        for (int i = 0; i < colunas.length; i++) {
+            alunoJTable.getColumnModel().getColumn(i).setHeaderValue(colunas[i]);
+        }
     }
 
     public static void main(String[] args) {
