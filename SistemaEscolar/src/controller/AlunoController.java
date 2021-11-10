d4eeb93af6bb6846f9176b612c18317bfaf773e3
Jackson <lordjackson@gmail.com>
2021-11-10 15:47:37 -0300

    Finalização do CRUD - Funções Editar, Deletar e Buscar

--- SistemaEscolar/src/controller/AlunoController.java
+++ SistemaEscolar/src/controller/AlunoController.java
@@ -58,4 +58,67 @@ public class AlunoController {
             return null;
         }
     }
+
+    public void atualizar(Aluno aluno) {
+        try {
+            String sql;
+            sql = "UPDATE aluno SET nome = ?, matricula = ?, dt_nascimento = ? WHERE id = ?";
+
+            PreparedStatement stmt = bd.getConexao().prepareStatement(sql);
+
+            stmt.setInt(4, aluno.getId());
+            stmt.setString(1, aluno.getNome());
+            stmt.setString(2, aluno.getMatricula());
+            stmt.setString(3, aluno.getDt_nascimento());
+            stmt.execute();
+            stmt.close();
+
+        } catch (SQLException u) {
+            throw new RuntimeException(u);
+        }
+    }
+
+    public void remover(int id) {
+        try {
+            System.out.println("id: " + id);
+            String sql;
+            if (id != 0) {
+                sql = "DELETE FROM aluno WHERE id = ?";
+                PreparedStatement stmt = bd.getConexao().prepareStatement(sql);
+
+                stmt.setInt(1, id);
+                stmt.execute();
+                stmt.close();
+            } else {
+                throw new RuntimeException("Selecione um registro para ser deletado");
+            }
+        } catch (SQLException u) {
+            throw new RuntimeException(u);
+        }
+    }
+
+    public ResultSet buscar(Aluno aluno) {
+        try {
+            String sql = "";
+            if (!aluno.getNome().isEmpty()) {
+                sql = "SELECT * FROM aluno WHERE nome LIKE '%" + aluno.getNome() + "%' ";
+
+            } else if (!aluno.getMatricula().isEmpty()) {
+                sql = "SELECT * FROM aluno WHERE matricula LIKE '%" + aluno.getMatricula() + "%' ";
+
+            } else if (!aluno.getDt_nascimento().isEmpty()) {
+                sql = "SELECT * FROM aluno WHERE dt_nascimento LIKE '%" + aluno.getDt_nascimento() + "%' ";
+            }
+
+            PreparedStatement ps = bd.getConexao().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            return rs;
+
+        } catch (SQLException e) {
+            e.getMessage();
+            JOptionPane.showMessageDialog(null, "Erro preencher o ArrayList");
+            return null;
+        }
+    }
 }
--- SistemaEscolar/src/view/AlunoFormList.java
+++ SistemaEscolar/src/view/AlunoFormList.java
@@ -7,11 +7,13 @@ import net.proteanit.sql.DbUtils;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.ResultSet;
 
 public class AlunoFormList {
     private JPanel panelMain;
-    private JButton salvarBtn;
+    private JButton salvarOuEditarBtn;
     private JButton cancelarBtn;
     private JTextField nomeTxt;
     private JTextField dt_nascimentoTxt;
@@ -19,32 +21,102 @@ public class AlunoFormList {
     private JButton listarBtb;
     private JTable alunoJTable;
     private JPanel panelTable;
+    private JTextField idTxt;
+    private JButton deletarBtn;
+    private JButton buscarBtn;
     String[] colunaNomes = {"Employee 1", "Employee 2"};
 
 
     public AlunoFormList() {
-        salvarBtn.addActionListener(new ActionListener() {
+
+        listar();
+        salvarOuEditarBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                AlunoController alunoController = new AlunoController();
                 Aluno aluno = new Aluno();
                 aluno.setNome(nomeTxt.getText());
                 aluno.setMatricula(matriculaTxt.getText());
                 aluno.setDt_nascimento(dt_nascimentoTxt.getText());
 
-                AlunoController alunoController = new AlunoController();
-                alunoController.salvar(aluno);
+                if (idTxt.getText().isEmpty()) {
+                    alunoController.salvar(aluno);
+                } else {
+                    aluno.setId(Integer.parseInt(idTxt.getText()));
+                    alunoController.atualizar(aluno);
+                }
 
                 //alunoController.testarConexao();
                 //System.out.println("Cliquei no botão");
+                listar();
+                limpar();
+
+                salvarOuEditarBtn.setText("Salvar");
             }
         });
         listarBtb.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                listar();
+            }
+        });
+        alunoJTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+                idTxt.setText(alunoJTable.getValueAt(alunoJTable.getSelectedRow(), 0).toString());
+
+                //Object objNome = alunoJTable.getValueAt(alunoJTable.getSelectedRow(), 1);
+                matriculaTxt.setText(alunoJTable.getValueAt(alunoJTable.getSelectedRow(), 1).toString());
+                nomeTxt.setText(alunoJTable.getValueAt(alunoJTable.getSelectedRow(), 2).toString());
+                dt_nascimentoTxt.setText(alunoJTable.getValueAt(alunoJTable.getSelectedRow(), 3).toString());
+
+                deletarBtn.setVisible(true);
+                salvarOuEditarBtn.setText("Editar");
+            }
+        });
+        deletarBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int i = JOptionPane.showConfirmDialog(
+                        null,
+                        "Deseja confirma a remoção do registro?"
+                );
+
+                if (i == JOptionPane.YES_OPTION && !idTxt.getText().isEmpty()) {
+
+                    AlunoController alunoController = new AlunoController();
+                    int id = Integer.parseInt(alunoJTable.getValueAt(alunoJTable.getSelectedRow(), 0).toString());
+
+                    alunoController.remover(id);
+                    listar();
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Selecione um registro para ser deletado");
+                }
+            }
+        });
+        cancelarBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                limpar();
+                listar();
+            }
+        });
+        buscarBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 AlunoController alunoController = new AlunoController();
-                ResultSet rs = alunoController.listar();
+
+                Aluno aluno = new Aluno();
+                aluno.setNome(nomeTxt.getText());
+                aluno.setMatricula(matriculaTxt.getText());
+                aluno.setDt_nascimento(dt_nascimentoTxt.getText());
+
+                ResultSet rs = alunoController.buscar(aluno);
 
                 alunoJTable.setModel(DbUtils.resultSetToTableModel(rs));
 
@@ -53,6 +125,23 @@ public class AlunoFormList {
         });
     }
 
+    public void limpar() {
+        idTxt.setText(null);
+        matriculaTxt.setText(null);
+        nomeTxt.setText(null);
+        dt_nascimentoTxt.setText(null);
+
+        salvarOuEditarBtn.setText("Salvar");
+    }
+
+    public void listar() {
+        AlunoController alunoController = new AlunoController();
+        ResultSet rs = alunoController.listar();
+
+        alunoJTable.setModel(DbUtils.resultSetToTableModel(rs));
+
+        setNomeColuna(new String[]{"ID", "Matricula", "Nome", "Data Nascimento"});
+    }
 
     public void setNomeColuna(String[] colunas) {
         for (int i = 0; i < colunas.length; i++) {
